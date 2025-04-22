ed747f245e5a17d1ee84a074383e4c1aa56c0e8a
Connor Baldes <connorbaldes@gmail.com>
2025-04-21 18:55:32 -0700

    Configured essential debian/ files

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/apt-world/issues
+# Bug-Submit: https://github.com/<user>/apt-world/issues/new
+# Changelog: https://github.com/<user>/apt-world/blob/master/CHANGES
+# Documentation: https://github.com/<user>/apt-world/wiki
+# Repository-Browse: https://github.com/<user>/apt-world
+# Repository: https://github.com/<user>/apt-world.git
